74f3af2878306b5f0a265b5ea7a9484447b7b054
Can Anil <can.anil@hotmail.com>
2017-05-19 12:07:16 +0100

    Create index.js
    init

---
+++ src/constants/index.js
@@ -0,0 +1,10 @@
+//API URLs
+const ROOT_URL = 'https://swapi.co/api/';
+
+//export const API_PEOPLE_URL = `${ROOT_URL}/people/`;
+export const API_PEOPLE_URL = 'https://swapi.co/api/people/';
+
+
+//Actions
+export const FETCH_PEOPLE = 'FETCH_PEOPLE';
+export const SELECT_PEOPLE = 'SELECT_PEOPLE';
