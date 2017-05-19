e3633115e82b785fdfe8b6c1aa079f1c834cc0bc
Can Anil <can.anil@hotmail.com>
2017-05-19 12:06:40 +0100

    Create index.js
    init

---
+++ src/components/People/index.js
@@ -0,0 +1,17 @@
+import React, { Component, PropTypes } from 'react';
+
+class People extends Component {
+  render() {
+    return (
+      <div>
+        this is people component -index.js
+      </div>
+    );
+  }
+}
+
+People.propTypes = {
+  //children: PropTypes.element
+};
+
+export default People;
