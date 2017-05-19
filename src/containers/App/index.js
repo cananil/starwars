861283d2be6f864a319da627b8733d108ff57039
Can Anil <can.anil@hotmail.com>
2017-05-19 12:08:33 +0100

    Create index.js
    init

---
+++ src/containers/App/index.js
@@ -0,0 +1,25 @@
+import React, { Component, PropTypes } from 'react';
+import { connect } from 'react-redux';
+import People from '../People';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <h1 className="display-1">
+          Starwars - May the force be with you
+        </h1>
+        <People />
+      </div>
+    );
+  }
+}
+
+App.propTypes = {
+  //children: PropTypes.element
+};
+
+People.propTypes = {
+
+}
+export default App;
